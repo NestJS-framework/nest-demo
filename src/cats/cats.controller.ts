2d3a76dbbf5b49abb6cc711f7d194c99f4e82a51
mahamd mesalm <mahamdabomesalm987@gmail.com>
2024-10-26 16:55:38 +0300

    Add validation to DTO fields and implement missing service method

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,33 +1,51 @@
 import { Cat } from './../interfaces/cat.interface';
-import { CreateCatDto } from './../create-cat.dto';
-import { Body, Controller, Get, Header, HostParam, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Query, Redirect, Req , Request} from '@nestjs/common';
+import { CreateCatDto } from '../create-cat.dto';
+import {
+  Body,
+  Controller,
+  Get,
+  Header,
+  HostParam,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+  Redirect,
+  Req,
+  Request,
+} from '@nestjs/common';
 import { CatsService } from './cats.service';
-
 
 @Controller('cats')
 export class CatsController {
-    constructor(private catsService: CatsService) {}
-
-    @Post()
-    async create(@Body() createCatDto: CreateCatDto) {
-      this.catsService.create(createCatDto);
-    }
+  constructor(private catsService: CatsService) {}
   
-    @Get()
-    async findAll(): Promise<Cat[]> {
-      return this.catsService.findAll();
-    }
 
-    @Get(':id')
-async findOne(@Param('id', ParseIntPipe) id: number) {
-  // return this.catsService.findOne(id);
-}
+  @Post()
+  async create(@Body() createCatDto: CreateCatDto){
+    return this.catsService.create(createCatDto);
+  }
 
-@Get(':id')
-async findOne2(
-  @Param('id', new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }))
-  id: number,
-) {
-  // return this.catsService.findOne(id);
-}
+  @Get()
+  async findAll(): Promise<Cat[]> {
+    return this.catsService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.catsService.findOne(id);
+  }
+
+  @Get(':id')
+  async findOne2(
+    @Param(
+      'id',
+      new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }),
+    )
+    id: number,
+  ) {
+    // return this.catsService.findOne(id);
+  }
 }
